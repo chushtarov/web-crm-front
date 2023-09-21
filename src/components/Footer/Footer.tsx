0499fda9d27cda5c79750ba01309de1ce4e0572d
chushtarov <kenlol@bk.ru>
2023-09-21 14:02:32 +0300

    дабавил auth хеадер

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+        © 2023 Team One: intocode
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -73,6 +73,23 @@ const Header = () => {
             >
               Экзамен
             </button>
+            {openMenu ? (
+          <div className={style.menu}>
+            <button>
+              <FaInfo className={style.icon} />
+              <Link className={style.link__color} to={"info"}>
+                Информация
+              </Link>
+            </button>
+            <button>
+              <MdAssignmentAdd className={style.icon} />
+              <Link className={style.link__color} to={"form"}>
+                {" "}
+                Записаться
+              </Link>
+            </button>
+          </div>
+        ) : null}
           </li>
           <li>
             <Link className={style.ul__link} to={"/"}>
@@ -95,23 +112,7 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        {openMenu ? (
-          <div className={style.menu}>
-            <button>
-              <FaInfo className={style.icon} />
-              <Link className={style.link__color} to={"info"}>
-                Информация
-              </Link>
-            </button>
-            <button>
-              <MdAssignmentAdd className={style.icon} />
-              <Link className={style.link__color} to={"form"}>
-                {" "}
-                Записаться
-              </Link>
-            </button>
-          </div>
-        ) : null}
+        
       </div>
       <div className={style.prof}>
         {token ? <div className={style.log__user}>{userOne.login}</div> : ""}
