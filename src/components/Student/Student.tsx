214a9b22d5e65127f57f604e37d8d0b3f69d3e71
ramzan <93776325+ramzanka06@users.noreply.github.com>
2023-09-25 16:06:57 +0300

    Доработка списка студентов

--- src/components/Student/Student.tsx
+++ src/components/Student/Student.tsx
@@ -7,11 +7,20 @@ import { AiOutlineUsergroupAdd } from 'react-icons/ai'
 const Student = () => {
   const [open, setOpen] = useState(false);
   return (
-    <form action="">
     <div className={style.student}>
       <div className={style.student_container}>
-        <div className={style.student_content}>
-          <h2 className={style.add_student}>Добавление нового студента</h2>
+        <div className={style.student_content}>       
+            <div className={style.list_student}>
+                  <div className={style.close_list}>
+                    
+                    <h2>Студенты</h2>
+                   <div>
+                    <div className={style.list_content}>
+                      <button onClick={() => setOpen(!open)} className={style.add}><AiOutlineUsergroupAdd/></button>
+                      {open ? <div className={style.add_stud}>
+                        <div className={style.block_stud}>
+                        <div style={{display: 'flex', justifyContent: 'flex-end', marginRight: '20px', marginTop: '20px'}}><button className={style.close_modal} onClick={() => setOpen(!open)}><RiCloseCircleLine/></button></div>
+                        <h2 className={style.add_student}>Добавление нового студента</h2>
           <div className={style.student_group_name}>
             <div>
             <p>Группа</p>
@@ -32,16 +41,12 @@ const Student = () => {
               <input placeholder="Password" className={style.input_login} type="text" />
             </div>
             </div>
-            <div className={style.student_button}>
-                <button onClick={() => setOpen(!open)} className={style.student_btn}>Добавить</button>
-                {open ? <div className={style.list_student}>
-                  <div className={style.close_list}>
-                    <h2>Студенты</h2>
-                   <div>
-                    <div className={style.list_content}>
-                      <button className={style.add}><AiOutlineUsergroupAdd/></button>
+            <div className={style.add_btn}>
+            <button className={style.adds}>Добавить</button>
+            </div>
+            </div>
+                      </div> : null}
                       <input className={style.search_student} placeholder="Search" type="text" />
-                      <button className={style.close_modal} onClick={() => setOpen(!open)}><RiCloseCircleLine/></button>
                   </div>
                    </div>
                   </div>
@@ -52,12 +57,10 @@ const Student = () => {
                       <p className={style.student_group}>bootcamp#18</p>
                     </div>
                   </div>                
-                </div> : null}
+                </div>
             </div>
         </div>
       </div>
-    </div>
-    </form>
   );
 };
 
